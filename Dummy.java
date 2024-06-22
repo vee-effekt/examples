812dc2159b3a0f8233533937c83b2d4aab6fb361
Thia Richey <gannet@cs.washington.edu>
2024-06-21 18:18:02 -0700

    second commit

--- Dummy.java
+++ Dummy.java
@@ -1,7 +1,7 @@
 public class Dummy {
     public static void main(String[] args) {
-        int x = 5;
-        for (int i = 0; i < x; i++) {
+        int y = 5;
+        for (int i = 0; i < y; i--) {
             // ... 
         }
     }
