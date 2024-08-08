e2fab7541a41894637b667708c7316ca1aab41d6
Thia Richey <gannet@cs.washington.edu>
2024-08-07 18:48:51 -0700

    delete all

--- Implementation.java
+++
@@ -1,6 +0,0 @@
-public class Implementation implements Interface{
-    @Override
-    public void foo_tag(int x_prime, boolean y) {
-
-    }
-}
--- Interface.java
+++
@@ -1,3 +0,0 @@
-public interface Interface {
-    void foo_tag(int x_prime, boolean y);
-}
