7608d69c8d4d368b1ae01a028e440830b561fee0
Thia Richey <gannet@cs.washington.edu>
2024-08-07 19:05:19 -0700

    new

--- File.java
+++ File.java
@@ -1,6 +1,5 @@
 public class File {
     public static void main(String[] args) {
-        Any23DataFormat df = new Any23DataFormat();
-        df.setBaseUri(baseUri);
+        String s = new String().set("A").concat("B");
     }
 }
