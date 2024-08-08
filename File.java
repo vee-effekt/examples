be9c9e62ebfa0fa3f4c1380b8b6d6a0cbe22853f
Thia Richey <gannet@cs.washington.edu>
2024-08-07 19:21:29 -0700

    new

--- File.java
+++ File.java
@@ -1,5 +1,6 @@
 public class File {
     public static void main(String[] args) {
-        String s = new String().set("A");
+        String s = new String()
+                            .set("A");
     }
 }
