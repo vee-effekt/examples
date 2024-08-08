6bcf1d1faa9b1150c95f09cb458762fb047e00e4
Thia Richey <gannet@cs.washington.edu>
2024-08-07 19:00:07 -0700

    new

---
+++ File.java
@@ -0,0 +1,5 @@
+public class File {
+    public static void main(String[] args) {
+        Any23DataFormat df = new Any23DataFormat().setBaseURI(baseURI).setOutputFormat(Any23OutputFormat.RDF4JMODEL);
+    }
+}
