eb56bcf7be06a8c0860d737db61edb1987a0900a
Thia Richey <gannet@cs.washington.edu>
2024-08-07 19:05:45 -0700

    new

--- File.java
+++ File.java
@@ -1,5 +1,7 @@
 public class File {
     public static void main(String[] args) {
-        String s = new String().set("A").concat("B");
+        String s = new String();
+        s.set("A");
+        s.concat("B");
     }
 }
